5a8cb27fca330f3851376f992b4d1be6b276ea62
Dmitriy Murga <dm.murga@gmail.com>
2021-12-20 19:11:02 +0300

    aws.tf updated

--- aws.tf
+++ aws.tf
@@ -41,6 +41,12 @@ resource "aws_security_group" "allow-tomcat" {
     to_port   = 8080
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     protocol  = "-1"
     from_port = 0
